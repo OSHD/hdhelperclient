e9548259288f09a717adc727e9f6f8413479aa78
Brainfree <Meischtro@hotmail.com>
2015-10-22 17:33:16 -0700

    Implemented ground items

---
+++ src/com/hdhelper/agent/mod/GroundItemMod.java
@@ -0,0 +1,7 @@
+package com.hdhelper.agent.mod;
+
+/**
+ * Created by Jamie on 10/22/2015.
+ */
+public class GroundItemMod {
+}
---
+++ src/com/hdhelper/peer/RSGroundItem.java
@@ -0,0 +1,7 @@
+package com.hdhelper.peer;
+
+/**
+ * Created by Jamie on 10/22/2015.
+ */
+public interface RSGroundItem {
+}
