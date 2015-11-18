999f4d18832d7aae09de6287a6cff8c2d98648b1
Brainfree <Meischtro@hotmail.com>
2015-11-17 18:42:39 -0800

    Removed old client sources that were left in the root module

--- src/com/hdhelper/api/plugin/PluginStub.java
+++
@@ -1,9 +0,0 @@
-package com.hdhelper.api.plugin;
-
-import java.io.File;
-
-public interface PluginStub {
-
-    File getDataBase();
-
-}
--- src/com/hdhelper/api/runescript/Dumper.java
+++
@@ -1,7 +0,0 @@
-package com.hdhelper.api.runescript;
-
-/**
- * Created by Jamie on 11/16/2015.
- */
-public class Dumper {
-}
