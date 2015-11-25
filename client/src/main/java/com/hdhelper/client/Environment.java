9a2ce98de7c604872c5f903d55dfbec9bd106a19
Brainfree <Meischtro@hotmail.com>
2015-11-24 22:41:28 -0800

    Updated revision

--- client/src/main/java/com/hdhelper/client/Environment.java
+++ client/src/main/java/com/hdhelper/client/Environment.java
@@ -19,7 +19,7 @@ public class Environment {
     public static final int WORLD = 2;
 
 
-    public static int CLIENT_REVISION = 98;
+    public static int CLIENT_REVISION = 100;
 
     public static boolean RENDER_LANDSCAPE = true;
 
